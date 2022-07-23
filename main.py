e0309f7e215f988bdb4dbfcd52ee73c0047cb0ab
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2022-07-23 14:24:30 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import openpyxl
+filename="C:\\Users\prana\\OneDrive\\Documents\\Gant Chart Data\\DD.CrewList.CR.Jan 15_2022_as of Jan 27_2022.xlsx"
+crewsheet="Dino Daycare crew"
+book=openpyxl.load_workbook(filename)
+sheet=book[crewsheet]
